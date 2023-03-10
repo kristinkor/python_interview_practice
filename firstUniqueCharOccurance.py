5449e67a31e56989053155d332dbd2fe9c6f3103
korzhenevskaya <kristinkor@gmail.com>
2023-03-10 18:55:15 -0500

    corrected errors made in previous comment

--- firstUniqueCharOccurance.py
+++ firstUniqueCharOccurance.py
@@ -4,10 +4,10 @@ def firstUniqChar( s):
     :rtype: int
     """
     
-    for i in range(0,len(s)-1):
+    for i in range(0,len(s)):
         
         count = 0
-        for j in range(i, len(s)):
+        for j in range(0, len(s)):
             if(s[i] == s[j]):
                 count +=1
         if(count == 1):
